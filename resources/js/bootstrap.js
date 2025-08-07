1fb7f7353fc6b7abadccaf59e7d660b9da959e74
Andrew De Forest <andydeforest@gmail.com>
2025-08-07 10:59:59 -0700

    modified axios token behavior, removed redundant token

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,4 +2,4 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-axios.defaults.headers.common['Authorization'] = `Bearer ${import.meta.env.VITE_ADMIN_TOKEN}`;
+axios.defaults.withCredentials = true;
